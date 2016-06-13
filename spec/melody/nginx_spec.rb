b58d08f15bfa622020be16525d425af8e1fde435
hamaokah <strodr@gmail.com>
2016-06-14 01:23:45 +0900

    add nginx spec.rb

---
+++ spec/melody/nginx_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+# Check if nginx is installed
+describe package('nginx') do
+  it { should be_installed }
+end
+
+# Check if file exists
+['/etc/nginx/nginx.conf'].each do |file|
+  describe file(file) do
+    it { should be_file }
+  end
+end
+
+# Check if nginx is enabled
+describe service('nginx') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+# Check if port 80 is listening
+describe port(80) do
+  it { should be_listening }
+end
