a1a4f0cb25b3f4676ceed48d36fdaa52b0b13c32
hamaokah <strodr@gmail.com>
2016-06-18 23:00:08 +0900

    add: Check installed package version

--- spec/melody/package_spec.rb
+++ spec/melody/package_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
+# Check what packages are installed
 %w{nginx git}.each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
+end
+
+# Check installed package version
+describe command('nginx -v') do
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should match /1.4.6/ }
 end
