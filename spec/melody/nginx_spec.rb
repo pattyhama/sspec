e56acf646db1ecd2be3edeb09541e970b699880c
hamaokah <strodr@gmail.com>
2016-06-18 23:00:55 +0900

    add: Check if HTTP status code 200 is returned

--- spec/melody/nginx_spec.rb
+++ spec/melody/nginx_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-# Check if nginx is installed
-describe package('nginx') do
-  it { should be_installed }
-end
-
 # Check if file exists
 ['/etc/nginx/nginx.conf'].each do |file|
   describe file(file) do
@@ -21,4 +16,9 @@ end
 # Check if port 80 is listening
 describe port(80) do
   it { should be_listening }
+end
+
+# Check if HTTP status code 200 is returned
+describe command('curl http://127.0.0.1 -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
 end
