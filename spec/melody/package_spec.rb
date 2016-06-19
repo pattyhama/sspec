6724781ac4532c757497b00cf3eb7c9b114a006e
pattyhama <strodr@gmail.com>
2016-06-19 21:05:23 +0900

    add: chef package version check

--- spec/melody/package_spec.rb
+++ spec/melody/package_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
 # Check installed packages
-%w{nginx git foodcritic}.each do |pkg|
+%w{chef nginx git foodcritic}.each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
 end
 
 # Check installed package version
+describe command('chef-solo -v') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /11.8.2/ }
+end
+
 describe command('nginx -v') do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should match /1.4.6/ }
