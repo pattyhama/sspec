9961e8ae3cefe58e1115e9bf92d09097c84c8f01
pattyhama <strodr@gmail.com>
2016-06-19 20:39:11 +0900

    add: version check for git and foodcritic

--- spec/melody/package_spec.rb
+++ spec/melody/package_spec.rb
@@ -12,3 +12,13 @@ describe command('nginx -v') do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should match /1.4.6/ }
 end
+
+describe command('git --version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /1.9.1/ }
+end
+
+describe command('foodcritic -V') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /3.0.3/ }
+end
