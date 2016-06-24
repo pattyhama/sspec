f21e70b384fdb03f1dfa604bed7d6baf417025f8
pattyhama <strodr@gmail.com>
2016-06-24 21:20:43 +0900

    add traceroute test

--- spec/melody/package_spec.rb
+++ spec/melody/package_spec.rb
@@ -9,23 +9,23 @@ end
 
 # Check installed package version
 describe command('chef-solo -v') do
-#  its(:exit_status) { should eq 0 }
   its(:stdout) { should match /11.8.2/ }
 end
 
 describe command('nginx -v') do
-#  its(:exit_status) { should eq 0 }
   its(:stderr) { should match /1.4.6/ }
 end
 
 describe command('git --version') do
-#  its(:exit_status) { should eq 0 }
   its(:stdout) { should match /1.9.1/ }
 end
 
 describe command('foodcritic -V') do
-#  its(:exit_status) { should eq 0 }
   its(:stdout) { should match /3.0.3/ }
+end
+
+describe command('traceroute -V') do
+  its(:stderr) { should match /2.0.20/ }
 end
 
 describe package('rubocop') do
