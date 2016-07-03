e601b6b2f54b76228202a419bce6a30e7df4c5da
pattyhama <strodr@gmail.com>
2016-07-03 10:14:06 +0900

    add before_install

--- solo.rb
+++ solo.rb
@@ -1,9 +1,3 @@
 file_cache_path "/tmp/chef-solo"
 cookbook_path ["/root/chef-repo/cookbooks"]
 # log_level :debug
-
-# avoid problems with locales on chef
-# workaround https://github.com/opscode/chef/issues/2695
-ENV['LANG'] = 'en_US.UTF-8'
-ENV['LANGUAGE'] = ENV['LANG']
-ENV['LC_ALL'] = ENV['LANG']
