8085a2e352a198f27f462b65f6d518439548ceb8
pattyhama <strodr@gmail.com>
2016-07-03 12:57:49 +0900

    test as installed via gem

--- spec/melody/package_spec.rb
+++ spec/melody/package_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 # Check installed packages
-%w(chef traceroute foodcritic rubocop nginx).each do |pkg|
+%w(chef traceroute nginx).each do |pkg|
   describe package(pkg) do
     it { should be_installed }
   end
+end
+
+describe package('rubocop') do
+  it { should be_installed.by('gem').with_version('0.41.1') }
+end
+
+describe package('foodcritic') do
+  it { should be_installed.by('gem').with_version('6.3.0') }
 end
