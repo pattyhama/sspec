e99d2d2e15376933e69b296983d0354e7188762a
pattyhama <strodr@gmail.com>
2016-07-09 23:29:00 +0900

    modify to apache

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,6 +1,8 @@
-name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name 'apache'
+issues_url 'https://github.com/pattyhama/sspec/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/pattyhama/sspec' if respond_to?(:source_url)
+maintainer       'Harumi Hamaoka'
+maintainer_email 'strodr@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
