16ad33857bc75bd5399a5d09b2aa442c529c5777
pattyhama <strodr@gmail.com>
2016-07-10 09:48:30 +0900

    add apache2 test

--- spec/melody/apache_spec.rb
+++ spec/melody/apache_spec.rb
@@ -6,3 +6,28 @@ require 'spec_helper'
     it { should be_installed }
   end
 end
+
+# Check if apache2 is running/enabled
+describe service('apache2') do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+# Check if port 80 is listening
+describe port(80) do
+  it { should be_listening }
+end
+
+# Check if file exists
+['/var/www/index.html'].each do |file|
+ describe file(file) do
+   it { should be_file }
+   it { should be_mode 644 }
+   it { should contain 'hello apache' }
+ end
+end
+
+# Check if HTTP status code 200 is returned
+describe command('curl http://127.0.0.1 -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
+end
